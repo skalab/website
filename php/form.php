d976288f371c0519245fc0623f09ce86e147e315
Gael Barbier <barbier.gael@free.fr>
2019-09-11 13:24:15 +0200

    Added a minimal protection of form

--- php/form.php
+++ php/form.php
@@ -1,21 +1,35 @@
 <?php
 
-$email = $_POST["email"];
+if (!isset($_POST["email"]))
+{
+	echo "Non.";
+	exit();
+}
+
+$email = htmlspecialchars($_POST["email"]);
+$ip = getenv("REMOTE_ADDR");
  
 $EmailTo = "gael@skalab.fr";
-$Subject = "Skalab: nouveau contact";
+$Subject = "Skalab: nouveau contact de " + $email;
 
 $Body .= "Email: ";
 $Body .= $email;
+$Body .= "\n";
+
+$Body .= "From: ";
+$Body .= $ip;
 $Body .= "\n";
 
 // send email
 $success = mail($EmailTo, $Subject, $Body, "From:".$email);
  
 // redirect to success page
-if ($success){
+if ($success)
+{
    echo "success";
-}else{
+}
+else
+{
     echo "invalid";
 }
  
